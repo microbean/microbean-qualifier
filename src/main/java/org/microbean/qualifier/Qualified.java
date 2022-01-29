d2111079e41219f0a0e69f42e93ecfd07749b1a9
Laird Nelson <ljnelson@gmail.com>
2022-01-29 15:11:06 -0800

    Changed the signature of Qualifiers
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- src/main/java/org/microbean/qualifier/Qualified.java
+++ src/main/java/org/microbean/qualifier/Qualified.java
@@ -29,21 +29,47 @@ import org.microbean.development.annotation.Experimental;
 
 import static java.lang.constant.ConstantDescs.BSM_INVOKE;
 import static java.lang.constant.ConstantDescs.CD_Object;
+import static java.lang.constant.ConstantDescs.DEFAULT_NAME;
 import static java.lang.constant.ConstantDescs.NULL;
 import static java.lang.constant.DirectMethodHandleDesc.Kind.STATIC;
 
 import static org.microbean.qualifier.ConstantDescs.CD_Qualified;
 import static org.microbean.qualifier.ConstantDescs.CD_Qualifiers;
 
+/**
+ * A {@link Constable} pairing of a {@link Qualifiers} and a thing
+ * that is qualified by them.
+ *
+ * @param <T> the type of the thing that is qualified
+ *
+ * @param qualifiers the {@link Qualifiers}; must not be {@code null}
+ *
+ * @param qualified the thing being qualified; may be {@code null}
+ *
+ * @author <a href="https://about.me/lairdnelson"
+ * target="_parent">Laird Nelson</a>
+ *
+ * @see Qualifiers
+ */
 @Experimental
-public record Qualified<T>(Qualifiers<?> qualifiers, T qualified) implements Constable {
+public record Qualified<T>(Qualifiers<?, ?> qualifiers, T qualified) implements Constable {
 
 
   /*
    * Canonical constructor.
    */
 
-  
+
+  /**
+   * Creates a new {@link Qualified}.
+   *
+   * @param qualifiers the {@link Qualifiers}; must not be {@code null}
+   *
+   * @param qualified the thing being qualified; may be {@code null}
+   *
+   * @exception NullPointerException if {@code qualifiers} is {@code
+   * null}
+   */
   public Qualified {
     qualifiers = Objects.requireNonNull(qualifiers, "qualifiers");
   }
@@ -53,7 +79,30 @@ public record Qualified<T>(Qualifiers<?> qualifiers, T qualified) implements Con
    * Instance methods.
    */
 
-  
+
+  /**
+   * Returns an {@link Optional} containing the nominal descriptor for
+   * this instance, if one can be constructed, or an {@linkplain
+   * Optional#isEmpty() empty} {@link Optional} if one cannot be
+   * constructed.
+   *
+   * <p>This method will return an {@linkplain Optional#isEmpty()
+   * empty} {@link Optional} if the return value of {@link
+   * #qualified()} is not {@code null} and is not an instance of
+   * {@link Constable}.</p>
+   *
+   * @return an {@link Optional} containing the nominal descriptor for
+   * this instance, if one can be constructed, or an {@linkplain
+   * Optional#isEmpty() empty} {@link Optional} if one cannot be
+   * constructed; never {@code null}
+   *
+   * @nullability This method never returns {@code null}.
+   *
+   * @idempotency This method is idempotent and deterministic.
+   *
+   * @threadsafety This method is safe for concurrent use by multiple
+   * threads.
+   */
   @Override // Constable
   public final Optional<? extends ConstantDesc> describeConstable() {
     final Optional<? extends ConstantDesc> qualifiersDesc = this.qualifiers().describeConstable();
@@ -69,26 +118,15 @@ public record Qualified<T>(Qualifiers<?> qualifiers, T qualified) implements Con
       }
       if (qualifiedDesc.isPresent()) {
         return
-          Optional.of(DynamicConstantDesc.of(BSM_INVOKE,
-                                             MethodHandleDesc.ofMethod(STATIC,
-                                                                       CD_Qualified,
-                                                                       "of",
-                                                                       MethodTypeDesc.of(CD_Qualified,
-                                                                                         CD_Qualifiers,
-                                                                                         CD_Object))));
+          Optional.of(DynamicConstantDesc.ofNamed(BSM_INVOKE,
+                                                  DEFAULT_NAME,
+                                                  CD_Qualified,
+                                                  MethodHandleDesc.ofConstructor(CD_Qualified,
+                                                                                 CD_Qualifiers,
+                                                                                 CD_Object)));
       }
     }
     return Optional.empty();
-  }
-
-
-  /*
-   * Static methods.
-   */
-
-  
-  public static final <T> Qualified<T> of(final Qualifiers<?> qualifiers, final T qualified) {
-    return new Qualified<>(qualifiers, qualified);
   }
 
 }
