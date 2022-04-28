2911abe08de6ba60dde33c952a7a72dc97c738dc
Laird Nelson <ljnelson@gmail.com>
2022-04-28 15:06:11 -0700

    Introduces Bindings.java (#17)
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- src/test/java/org/microbean/qualifier/TestConstableSemantics.java
+++ src/test/java/org/microbean/qualifier/TestConstableSemantics.java
@@ -21,9 +21,13 @@ import java.lang.constant.ConstantDesc;
 
 import java.lang.invoke.MethodHandles;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 final class TestConstableSemantics {
 
@@ -32,9 +36,28 @@ final class TestConstableSemantics {
   }
 
   @Test
+  final void testQualifiedRecordClassName() {
+    assertEquals("org.microbean.qualifier.Qualified$Record", Qualified.Record.class.getName());
+  }
+
+  @Test
   final void testConstableSemantics() throws ReflectiveOperationException {
     final Qualifiers<String, String> q = Qualifiers.of("a", "b", "c", "d");
     assertEquals(q, q.describeConstable().orElseThrow().resolveConstantDesc(MethodHandles.lookup()));
   }
-  
+
+  @Test
+  final void testDeliberateLossOfInfo() throws ReflectiveOperationException {
+    final Qualifiers<String, String> q0 =
+      Qualifiers.of(Map.of("a", "b", "c", "d"),
+                    Map.of("w", "x", "y", "z"));
+    @SuppressWarnings("unchecked")
+    final Qualifiers<String, String> q1 =
+      (Qualifiers<String, String>)q0.describeConstable().orElseThrow().resolveConstantDesc(MethodHandles.lookup());
+    assertEquals(q0.toMap(), q1.toMap());
+    assertTrue(q1.info().isEmpty());
+    assertNotEquals(q0.info(), q1.info());
+    assertEquals(q0, q1);
+  }
+
 }
